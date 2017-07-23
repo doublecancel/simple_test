7800d392d471c12d6d245b86d6afcb218ec748f6
lxl <471567694@qq.com>
2017-07-23 18:15:12 +0800

    添加数据工具类

---
+++ add_data_utils/src/main/java/com/test/pyt/Main.scala
@@ -0,0 +1,45 @@
+package com.test.pyt
+
+import java.sql.{Connection, DriverManager, PreparedStatement}
+
+/**
+  * Created by Administrator on 2017/7/23 0023.
+  */
+class Main {
+
+}
+
+object Conn{
+
+  val driverName = "com.mysql.jdbc.Driver"
+  val url = "jdbc:mysql://localhost:3306/test?serverTimezone=GMT"
+  val user = "root"
+  val password = "root"
+  val table = "User"
+  case class Column(var fieldName : String, var fieldType : String, var isPrimary : Boolean = false)
+  var container : List[Column] = List()
+  val mappings : Map[String, Any] = Map("bigint"->Long, "varchar"->String, "datetime"->java.util.Date)
+
+  def getTableInfo = {
+    Class.forName(driverName)
+    val connection =  DriverManager.getConnection(url, user, password)
+    val pst = connection.prepareStatement("desc " + table)
+    val ret = pst.executeQuery();
+    while (ret.next){
+      container = container :+ Column(ret.getString("Field"), ret.getString("Type"))
+    }
+    ret.close()
+    pst.close()
+    connection.close()
+  }
+
+  def create = {
+
+  }
+
+  def main(args: Array[String]): Unit = {
+    getTableInfo
+    container.foreach(println(_))
+  }
+
+}
